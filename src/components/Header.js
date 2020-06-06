c04576dc797428e6da12385175f65e636e075b26
Ramiro Martin <ramiro@pop-os.localdomain>
2020-06-06 20:00:04 -0300

    modificando rm branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
   return (
     <div style={{ widht: "100%", textAlign: "center", margin: "30px" }}>
       <Fade timeout={2100} in={fadeIn}>
-        <h1>RM BRANCH</h1>
+        <h1>RM BRANCH 1</h1>
         <Link to="/">
           <img
             src={mainLogo}
